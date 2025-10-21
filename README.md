ba60880404f9b55f360944541b2569b9ee100c24
everviera12 <evertigre10@gmail.com>
2025-10-21 17:06:09 -0600

    feat(app.js): new route to get title and links

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 A simple Node.js API for scraping text or content from web pages using Axios and Cheerio.
 
+## Cheerio documentation
+
+<a href="https://cheerio.js.org/" target="_blank">
+  <img src="https://cheerio.js.org/img/orange-c.svg" alt="Cheerio Logo" width="80" />
+</a>
+
+<br />
+
 ## Getting Started
 
 Run the following commands to install all dependencies using `pnpm`:
@@ -9,12 +17,15 @@ Run the following commands to install all dependencies using `pnpm`:
 ```bash
 pnpm add express
 ```
+
 ```bash
 pnpm add axios
 ```
+
 ```bash
 pnpm add cheerio
 ```
+
 ```bash
 pnpm add cors
 ```
--- app.js
+++ app.js
@@ -1,31 +1,99 @@
 const express = require("express");
-const axios = require("axios");
 const cheerio = require("cheerio");
+const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
-const port = 8080
+const port = 8080;
 
-app.use(cors())
+/* app.use(cors()); */
 
 app.get('/', (req, res) => {
-    const routes = ["/load", "/extract"]
-    res.status(200).send({ message: "Test this routes!", routes })
-})
+    const html = `
+        <h1>Test this route!</h1>
+        <p>Click here to go to the scrape endpoint:</p>
+
+        <div style="display: grid;">
+            <a href="/load" style="text-decoration: none;">
+                Go to 
+                <span style="text-decoration: underline;">load</span> 
+                route
+            </a>
+
+            <a href="/extract" style="text-decoration: none;">
+                Go to 
+                <span style="text-decoration: underline;">extract</span> 
+                route
+            </a>
+
+            <a href="/extract-url" style="text-decoration: none;">
+                Go to 
+                <span style="text-decoration: underline;">extract</span> 
+                route
+            </a>
+        </div>
+    `;
+    res.status(200).send(html);
+});
 
 app.get('/load', async (req, res) => {
     const $ = cheerio.load("<h1 class='title'>Hello, world!</h1>");
     try {
-        const texto = $('h1.titlee').text();
+        const texto = $('h1.title').text();
         res.status(200).send({ message: "Scrape endpoint", data: texto });
     } catch (error) {
-        res.status(404).send({ message: "No data found" });
+        res.status(404).send({ message: "No data found", error: error.message });
     }
 });
 
+app.get("/extract", (req, res) => {
+    const $ = cheerio.load(`
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li class="blue sel">Three</li>
+            <li class="red">Four</li>
+            <li class="red">Five</li>
+            <li class="red">Six</li>
+        </ul>
+    `);
 
-app.get("/extract")
+    try {
+        const data = $.extract({ red: ['.red'], blue: ['.blue'] });
+        res.status(200).send({ message: "Scrape endpoint", data: data });
+    } catch (error) {
+        res.status(404).send({ message: "No data found", error: error.message });
+    }
+})
+
+app.get("/extract-url", async (req, res) => {
+    try {
+        const response = await axios.get("https://news.ycombinator.com/");
+        console.log(response);
+        
+        const html = response.data;
+
+        const $ = cheerio.load(html);
+
+        const titles = [];
+        $('span.titleline').each((index, element) => {
+            titles.push($(element).text());
+        });
+
+        const links = [];
+        $('span.titleline > a').each((index, element) => {
+            links.push($(element).attr('href'));
+        });
+
+        const data = titles.map((title, index) => ({ title: title, link: links[index] }));
+
+        res.status(200).send({ message: "Hacker News Titles", data: data });
+
+    } catch (error) {
+        res.status(404).send({ message: "No data found", error: error.message });
+    }
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
